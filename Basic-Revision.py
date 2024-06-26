7cb04f42d8a0952815af2d9d5b1378b121d1b3cd
Nirdosh Kushwaha <148872957+Nirdosh2004@users.noreply.github.com>
2024-06-26 18:55:10 +0530

    Create Basic-Revision.py

---
+++ Basic-Revision.py
@@ -0,0 +1,21 @@
+str1 = 'what is your name?'
+str2 = 'my name is Nirdosh Kushwaha'
+print(str1  )
+print(str2)
+
+str3 = "hello brother how are you ? \n I'm fine . Thankyou!"
+print(str3)
+
+
+it is called concatenated string (means adding two or more string together)
+one = 'One'
+two = ' Two'
+sum = one + two
+print(sum + one)
+
+
+printing the length of the string 
+print(len(two))
+
+
+
